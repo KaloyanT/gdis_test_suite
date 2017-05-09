a7e30ef3f44c3ac8e297bdc751c00f910fb65b91
nils <nils@065-176.eduroam.rwth-aachen.de>
2017-05-09 11:37:37 +0200

    added first endpoints post/get, however still thinking about switching to python

--- backend/api/src/main/java/com/gdis/api/Application.java
+++ backend/api/src/main/java/com/gdis/api/Application.java
@@ -4,18 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/{entryId}/action")
 public class Application {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@RequestMapping(method = RequestMethod.POST)
+	public String userIdInput(@PathVariable Integer entryId, @RequestBody String method) {
+		return Integer.toString(entryId) + " with action " + method;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String userId(@PathVariable Integer entryId) {
+		return Integer.toString(entryId);
+	}
 
 }
