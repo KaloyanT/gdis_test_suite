a7dd40918dbe5f8596ab4f271c276c85594695c0
Kaloyan kaloyan.todorov@rwth-aachen.de <kaloyan.todorov@rwth-aachen.de>
2017-06-19 22:31:16 +0200

    How To Properly Link Docker Containers

--- backend/exporter/src/main/java/com/gdis/exporter/util/DBClient.java
+++ backend/exporter/src/main/java/com/gdis/exporter/util/DBClient.java
@@ -3,7 +3,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
